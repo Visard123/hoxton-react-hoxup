6f863769da662e4e8885fd7c9aa49c624efaab7a
Visard123 <92378096+Visard123@users.noreply.github.com>
2022-01-20 00:20:15 +0100

    added and deleted some stuffs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-
-import { Route , Routes } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import MainApp from "./Pages/MainApp";
 export default function App() {
   return (
-    <Routes>
-<Route path={'/login'} element={<LoginPage/>}> </Route>
-    </Routes>
-  )
-   
-    
+    <div className="app">
+      <Routes>
+        <Route index element={<Navigate replace to="/login" />} />
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="logged-in " element={<MainApp />} />
+      </Routes>
+    </div>
+  );
+}
---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+function LoginPage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/users`)
+      .then((resp) => resp.json())
+      .then((userFromServer) => setUsers(userFromServer));
+    console.log(setUsers);
+  }, []);
+
+  function createNewUser(firstName, lastName, phoneNumber, avatar) {
+    return fetch(`http://localhost:4000/users`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        firsName: firstName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        avatar: avatar,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((newUser) => {});
+  }
+  return (
+    <div className="main-wrapper login">
+      <section className="login-section">
+        <h2>Choose your user!</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              <Link to={"/login"}>
+                <button className="user-selection">
+                  <img
+                    className="avatar"
+                    width="50"
+                    height="50"
+                    src={user.avatar}
+                    alt={user.firstName}
+                  />
+                  <h3>
+                    {user.firstName} {user.lastName}
+                  </h3>
+                </button>
+              </Link>
+            </li>
+          ))}
+
+          <li>
+            <button className="user-selection">
+              <h3>+ Add a new user</h3>
+            </button>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+export default LoginPage;
